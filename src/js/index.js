df1beacb2c809037ed83069e355d6c5b923c6a78
Matt Batman <batman.matt@gmail.com>
2017-04-16 10:15:01 -0400

    Fix request url typo, allow cross-origin requests on dev server

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 import * as d3 from "../../node_modules/d3";
 
-treasuryIo('SELECT * FROM t1 LIMIT 10', callback);
+treasuryIo('SELECT "name","sql" FROM sqlite_master', callback);
 
 function treasuryIo(query){
-  return d3.json('http//api.treasury.io/cc7znvq/47d80ae900e04f2/sql/?q='+query, callback);
+  return d3.json('http://api.treasury.io/cc7znvq/47d80ae900e04f2/sql/?q='+query, callback);
 }
 
 function callback(x) {
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,12 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'build/js')
-  }
+  },
+  devServer: {
+   headers: {
+     "Access-Control-Allow-Origin": "*",
+     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+     "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+   }
+ }
 }
